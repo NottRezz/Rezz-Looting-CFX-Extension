405d6ec498cb22f70e05d4f4a2c9c5befd9e077a
Ryker <RJE1503@gmail.com>
2026-03-23 19:29:54 -0600

    Initial Client Commit

---
+++ Config.cs
@@ -0,0 +1,43 @@
+﻿using CitizenFX.Core;
+using System.Collections.Generic;
+
+namespace Rezz_Looting_Client
+{
+    public class Config
+    {
+        public int MaxLoot { get; set; }
+        public Dictionary<int, LootArea> LootAreas { get; set; }
+        public Dictionary<string, List<LootDefinition>> LootTablesByType { get; set; }
+    }
+
+    public class LootArea
+    {
+        public int ZoneId { get; set; }
+        public Vector3 ZoneCoords { get; set; }
+        public Dictionary<int, LootAreaSpawnZones> LootSpawns { get; set; }
+        public float Radius { get; set; }
+        public string LootType { get; set; }
+        public int MaxLoot { get; set; }
+        public int SpawnChance { get; set; }
+        public int LootTier { get; set; }
+    }
+
+    public class LootAreaSpawnZones
+    {
+        public int MainZoneId { get; set; }
+        public int SubZoneId { get; set; }
+        public Vector3 SpawnCoords { get; set; }
+        public string ExclusiveType { get; set; }
+    }
+
+    public class LootDefinition
+    {
+        public string LootName { get; set; }
+        public string LootLabel { get; set; }
+        public string LootType { get; set; }
+        public int MinAmount { get; set; }
+        public int MaxAmount { get; set; }
+        public int MinTier { get; set; }
+        public int MaxTier { get; set; }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,63 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Debug = CitizenFX.Core.Debug;
+
+namespace Rezz_Looting_Client
+{
+    public class Main : BaseScript
+    {
+        private Config config;
+
+        public int CurrentZoneId = 0;
+        public LootArea CurrentZoneData;
+
+        public Main()
+        {
+            string json = API.LoadResourceFile(API.GetCurrentResourceName(), "config.json");
+            config = JsonConvert.DeserializeObject<Config>(json);
+
+            MainLoop();
+        }
+
+        private async void MainLoop()
+        {
+            while (true)
+            {
+                await BaseScript.Delay(5000);
+
+                int player = API.PlayerPedId();
+                Vector3 playerPos = API.GetEntityCoords(player, false);
+
+                if (CurrentZoneId == 0)
+                {
+                    foreach (var entry in config.LootAreas)
+                    {
+                        var zoneId = entry.Key;
+                        var zoneData = entry.Value;
+
+                        float dist = Vector3.Distance(zoneData.ZoneCoords, playerPos);
+
+                        if (dist <= zoneData.Radius)
+                        {
+                            CurrentZoneId = zoneId;
+
+                            Debug.WriteLine($"Entered zone {zoneId}");
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
